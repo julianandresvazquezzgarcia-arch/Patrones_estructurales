48c8a51c66751532550c8db2a13fc82a8671843c
julianandresvazquezzgarcia-arch <julianandresvazquezzgarcia@gmail.com>
2026-04-23 19:57:36 -0500

    Remove header comments from PersonajeDecorador.java
    Removed header comments from PersonajeDecorador.java

---
+++ src/PersonajeDecorador.java
@@ -0,0 +1,28 @@
+public abstract class PersonajeDecorador implements IPersonaje {
+
+    protected IPersonaje personaje;
+
+    public PersonajeDecorador(IPersonaje personaje) {
+        this.personaje = personaje;
+    }
+
+    @Override
+    public void recibirDano(int dano) {
+        personaje.recibirDano(dano);
+    }
+
+    @Override
+    public boolean estaVivo() {
+        return personaje.estaVivo();
+    }
+
+    @Override
+    public String getNombre() {
+        return personaje.getNombre();
+    }
+
+    @Override
+    public int getPuntosDeVida() {
+        return personaje.getPuntosDeVida();
+    }
+}
